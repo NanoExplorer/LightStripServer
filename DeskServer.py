f9080fc04d374ed14c8103faa7ea052affb58b1a
Christopher <ctr_287@sbcglobal.net>
2019-04-02 23:07:35 -0500

    trying out websockets!

--- DeskServer.py
+++ DeskServer.py
@@ -8,7 +8,7 @@ light strip CI port connected to rpi SCLK port.
 The raspberry pi must have hardware SPI kernel support (definitely works with
 Occidentalis, but might not work with default Wheezy.
 
-Starts a UDP server on port 12625 (configurable) and listens for data.
+Starts a websocket server on port 8765 (configurable) and listens for data.
 
 """
 import sys
@@ -23,13 +23,15 @@ ch.setLevel(logging.DEBUG)
 root.addHandler(ch)
 import time
 import socket
+import websockets
 import dropbox
 import DeskLogicThread
 import signal
+import asyncio
 
 #feel free to change these values
 IP = "0.0.0.0"
-PORT = 12625
+PORT = 8765
 
 def save_ip_address():
     s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
@@ -37,7 +39,7 @@ def save_ip_address():
     ip = s.getsockname()[0]
     with open('secret','r') as sf:
         token = sf.read()
-        #Dear future me: DON'T PUT THE GITHUB KEY INTO THE SOURCE CODE AND PUSH IT TO GH.
+        #Dear future me: DON'T PUT THE DROPBOX KEY INTO THE SOURCE CODE AND PUSH IT TO GH.
     dbclient = dropbox.Dropbox(token)
     dbclient.files_upload(str.encode(ip),'/ip_zeropi.txt',mode=dropbox.files.WriteMode('overwrite'))
 
@@ -48,31 +50,41 @@ def signal_handler(signal, frame):
     sys.exit(1)
 
 def redlight():
-    stuff="30:0:0".encode(encoding="UTF-8")
+    stuff="30:0:0"
     #30 is the lowest level of red. Set it any lower and it'll be off. I promise :)
     worker.sendMessage(stuff)
     while True:
         time.sleep(700)
-
+@asyncio.coroutine
+def rcv_color(websocket,path):
+    
+    # with socket.socket(socket.AF_INET, #internet
+    #                      socket.SOCK_STREAM) as sock: #TCP
+        # sock.bind((IP, PORT))
+        # sock.listen(1)
+        # while True:
+        #     conn, addr = sock.accept()
+    addr=websocket.remote_address[0]
+    logging.info("Connection established with {}.".format(addr))
+    while True:
+        try:
+            data = yield from websocket.recv() 
+            try:
+                worker.sendMessage(data)
+                logging.info("Received: {}".format(data))
+            except:
+                logging.warning("Received malformed data: {}".format(data))
+        except websockets.exceptions.ConnectionClosed:
+            logging.info("Lost connection with {}.".format(addr))
+            break
+    # conn.close()
+    # logging.info("Connection closed.")
 def main():
     logging.info("Server starting.")
-    with socket.socket(socket.AF_INET, #internet
-                         socket.SOCK_STREAM) as sock: #TCP
-        sock.bind((IP, PORT))
-        sock.listen(1)
-        while True:
-            conn, addr = sock.accept()
-            logging.info("Connection established with {}.".format(addr[0]))
-            while True:
-                data = conn.recv(20) #apparently "buffer size" is 20 bytes. Don't know how that will affect me
-                if not data: break
-                try:
-                    worker.sendMessage(data)
-                    logging.info("Received: {}".format(str(data)))
-                except:
-                    logging.warning("Received malformed data: {}".format(str(data)))
-            conn.close()
-            logging.info("Connection closed.")
+    start_server=websockets.serve(rcv_color,IP,PORT)
+    
+    asyncio.get_event_loop().run_until_complete(start_server)
+    asyncio.get_event_loop().run_forever()
 
 if __name__ == "__main__":
     try:    
